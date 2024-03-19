22c099b0516220e4f45febf2a5904853ed8543fc
Ukana.Bastreet <s.ppp@bk.ru>
2024-03-20 00:17:43 +0300

    some fixes

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,35 +1,37 @@
-import { useEffect, useRef, useState } from "react"
-import { io } from "socket.io-client"
-import useWindowDimensions from "../app/hooks/useDemensions"
-import canvasState from "../app/store/canvasState"
+import { useEffect, useRef } from "react";
+import useWindowDimensions from "../app/hooks/useDimensions";
+import canvasState from "../app/store/canvasState";
 
 const Canvas = () => {
-  const canvasRef = useRef(null)
-  const { width, height } = useWindowDimensions(canvasState.onCanvasResize)
+  const canvasRef = useRef(null);
+  const { width, height } = useWindowDimensions(canvasState.onCanvasResize);
 
   useEffect(() => {
     if (canvasRef.current) {
-      canvasState.setCanvas(canvasRef.current)
+      canvasState.setCanvas(canvasRef.current);
     }
-    window.canvasState = canvasState
-    canvasState.drawer.lineWidth = 50
-    canvasState.drawer.fillStyle = "red"
-    canvasState.drawer.strokeStyle = "rgba(13, 48, 102, 0.1)"
-    canvasState.drawer.lineCap = "round"
-    // const socket = io("http://localhost:3001")
+    window.canvasState = canvasState;
+    canvasState.drawer.lineWidth = 50;
+    canvasState.drawer.fillStyle = "red";
+    canvasState.drawer.strokeStyle = "rgba(13, 48, 102, 0.1)";
+    canvasState.drawer.lineCap = "round";
 
     return () => {
-      canvasState.destroy()
-      canvasRef.current.onmousedown = null
-      canvasRef.current.onmousemove = null
-      canvasRef.current.onmouseup = null
-    }
-  }, [])
+      clean(canvasRef, canvasState);
+    };
+  }, []);
 
   return (
     <div className="Canvas">
       <canvas id="canvas" ref={canvasRef} width={width} height={height} />
     </div>
-  )
+  );
+};
+
+function clean(canvasRef, canvasState) {
+  canvasState.destroy();
+  canvasRef.current.onmousedown = null;
+  canvasRef.current.onmousemove = null;
+  canvasRef.current.onmouseup = null;
 }
-export default Canvas
+export default Canvas;
