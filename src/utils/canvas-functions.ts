8eaae7bc9ba50af99d13c96e544e1f982f8c96dc
Ukana.Bastreet <s.ppp@bk.ru>
2024-03-29 15:50:34 +0300

    some fixes

--- src/utils/canvas-functions.ts
+++ src/utils/canvas-functions.ts
@@ -1,14 +1,7 @@
-export function getCursorPosition(canvas: HTMLCanvasElement, e: MouseEvent) {
-  // const rect = canvas.getBoundingClientRect()
-  // const x = e.clientX - rect.left
-  // const y = e.clientY - rect.top
-
+export function getCursorPosition(e: MouseEvent) {
   const target = e.target;
   const x = e.pageX - (target as HTMLElement).offsetLeft;
   const y = e.pageY - (target as HTMLElement).offsetTop;
-
-  // let x = e.clientX
-  // let y = e.clientY
 
   return [x, y];
 }
