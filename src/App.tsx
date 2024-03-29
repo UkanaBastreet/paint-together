6e0449625d742ca159efb23cbd4b8793d8647e43
Ukana.Bastreet <s.ppp@bk.ru>
2024-03-29 15:29:56 +0300

    recreate react app with ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
-import React from "react";
-import Canvas from "./components/Canvas.tsx";
-import Toolbar from "./components/Toolbar.tsx";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-const App = () => {
+function App() {
   return (
     <div className="App">
-      <Toolbar />
-      <Canvas />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
-};
+}
 
 export default App;
