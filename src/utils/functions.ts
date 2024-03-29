5e0b9e638f38f884f3690eebd6496217e45ed24b
Ukana.Bastreet <s.ppp@bk.ru>
2024-03-29 15:56:08 +0300

    add sass, create functions and hooks files

---
+++ src/utils/functions.ts
@@ -0,0 +1,8 @@
+export function getCursorPosition(e: MouseEvent) {
+    const target = e.target;
+    const x = e.pageX - (target as HTMLElement).offsetLeft;
+    const y = e.pageY - (target as HTMLElement).offsetTop;
+  
+    return [x, y];
+  }
+  
